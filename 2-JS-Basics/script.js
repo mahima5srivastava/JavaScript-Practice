e33836b72fed88f0159b1d222b06d21d5b0423ad
Mahima <mahima.srivastava@in.ibm.com>
2020-04-12 14:32:08 +0530

    Java script basics

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,75 @@
+/*console.log("Hello World!!!");
+var firstName = 'Mahima';
+console.log(firstName);
+
+ var lastName = "Srivastava";
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+age = "abc";
+console.log(age);
+age = true;
+console.log(age);
+age = null;
+console.log(age);
+var sum = age + 21;
+console.log(sum);
+var nothing;
+console.log(nothing);
+var nullVar = null;
+if (nullVar === nothing) {
+    console.log("null = nothing")
+} else {
+    console.log("null != nothing")
+}
+age = 26
+console.log(firstName + ' ' + age)
+var a, b, c = 5;
+a = 2
+b = 3
+console.log(a + " " +  b + " " + c);
+alert(a + " " +  b + " " + c)
+var lastName = prompt("What is his last name?")
+console.log(lastName);
+*/
+ var year = 2020
+var gappuAge = year - 1993;
+console.log(gappuAge);
+var sappuAge = year - 1991;
+
+//logical operator
+var mahimaOlder = gappuAge > sappuAge
+console.log(mahimaOlder)
+
+//typeOf operator
+
+console.log(typeof mahimaOlder)
+
+//operator precedence
+
+var now = 2020;
+var yearMahima = 1993
+var fullAge = 50
+var isFullAge = now - yearMahima >= fullAge
+console.log(isFullAge)
+var ageMahima = now - yearMahima
+var ageMark = 25
+var average = ageMahima + ageMark/2 
+console.log(average)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
